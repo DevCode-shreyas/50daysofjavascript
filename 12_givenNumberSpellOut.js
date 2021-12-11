16ed8ea4f4270dbe8de14d41efc9e5279037a1bd
shreyas <shreyasbole4@gmail.com>
2021-12-11 19:57:25 +0530

    new commit

--- 12_givenNumberSpellOut.js
+++ 12_givenNumberSpellOut.js
@@ -1,53 +1,87 @@
-const sayNumberInEnglish = (n) => {
-    const SINGLE_ARRAY = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    const TEEN_ARRAY = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
-    const DOUBLE_ARRAY = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-
-    //For Single Digit
-    if(n.toString().length === 1){
-        return SINGLE_ARRAY[n]
-    }
-
-    //For Double Digits
-    if(n.toString().length === 2){
-        if(parseInt(n.toString().charAt(n.length - 2)) === 1){
-            return TEEN_ARRAY[(n%10)]
-        }
-
-        if(n % 10 === 0){
-            return DOUBLE_ARRAY[(n/10) - 2]
-        }
-
-        const partOne = DOUBLE_ARRAY[Math.floor((n/10)) - 2]
-        const partTwo = SINGLE_ARRAY[n%10]
-
-        return partOne + "-" + partTwo
-    }
-
-    if(n%100 === 0 && parseInt(n.toString().charAt(0)) !== 0){
-        return SINGLE_ARRAY[(n/100)] + " hundred"
-    }
-
-    const remainingNumber =n - parseInt(${parseInt(n.toString().charAt(0))}00)
-
-    if(remainingNumber.toString().length === 1){
-        return SINGLE_ARRAY[Math.floor((n/100))] + " hundred " + SINGLE_ARRAY[remainingNumber]
-    }
-
-    if(parseInt(remainingNumber.toString().charAt(remainingNumber.length - 2)) === 1){
-        return SINGLE_ARRAY[Math.floor((n/100))] + " hundred " + TEEN_ARRAY[(remainingNumber%10)]
-    }
-
-    if(remainingNumber % 10 === 0){
-        return SINGLE_ARRAY[Math.floor((n/100))] + " hundred " + DOUBLE_ARRAY[(remainingNumber/10) - 2]
-    }
-
-    const partOne = DOUBLE_ARRAY[Math.floor((remainingNumber/10)) - 2]
-    const partTwo = SINGLE_ARRAY[remainingNumber%10]
-
-    return SINGLE_ARRAY[Math.floor((n/100))] + " hundred " + partOne + "-" + partTwo
+var NUMBER2TEXT = {
+  ones: [
+    "",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ],
+  tens: [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "fourty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ],
+  sep: [
+    "",
+    " thousand ",
+    " million ",
+    " billion ",
+    " trillion ",
+    " quadrillion ",
+    " quintillion ",
+    " sextillion ",
+  ],
 };
 
+const { ones, tens, sep } = NUMBER2TEXT;
 
+const sayNumberInEnglish = (number) => {
+  let arr = [],
+    str = "",
+    i = 0;
+  if (number.length === 0) {
+    return;
+  }
 
-console.log(`5635 in english is: ${sayNumberInEnglish(5635)}`)
+  number = parseInt(number, 10);
+  if (isNaN(number)) {
+    return;
+  }
+
+  while (number) {
+    arr.push(number % 1000);
+    number = parseInt(number / 1000, 10);
+  }
+
+  while (arr.length) {
+    str =
+      (function (a) {
+        var x = Math.floor(a / 100),
+          y = Math.floor(a / 10) % 10,
+          z = a % 10;
+
+        return (
+          (x > 0 ? ones[x] + " hundred " : "") +
+          (y >= 2 ? tens[y] + "-" + ones[z] : ones[10 * y + z])
+        );
+      })(arr.shift()) +
+      sep[i++] +
+      str;
+  }
+
+  return str;
+};
+
+console.log(`5635 in english is: ${sayNumberInEnglish(5635)}`);
--- 18_calGrainsOnASquareOnChessboard.js
+++ 18_calGrainsOnASquareOnChessboard.js
@@ -1,10 +1,16 @@
 const totalGrains = () => {
-    // Code here
+  // Code here
 
-    
-	return 
+  var ans = BigInt(2 ** 64);
+  return ans - 1n;
+};
 
-}
+const grainsOn = (input) => {
+  // Code here
 
-console.log(`Grains on 5th square: ${grainsOn(5)}`)
-console.log(`Total grains on the Chess Board: ${totalGrains()}`)
+  var ans = BigInt(2 ** (input - 1));
+  return ans;
+};
+
+console.log(`Grains on 5th square: ${grainsOn(62)}`);
+console.log(`Total grains on the Chess Board: ${totalGrains()}`);
