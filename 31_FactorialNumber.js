26d392815b1961a8b7ed7f02ab4cf44d5f57164c
shreyas <shreyasbole4@gmail.com>
2021-12-12 12:20:18 +0530

    new commit

---
+++ 31_FactorialNumber.js
@@ -0,0 +1,10 @@
+function factorial(n) {
+  // write your code here
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+let n = 4;
+console.log("The factorial of " + n + " is " + factorial(n));
