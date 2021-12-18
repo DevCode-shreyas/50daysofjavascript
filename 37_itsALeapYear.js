91f5241b95ae7a85ac7396d4d51f859fc979fe95
shreyas bole <68109462+DevCode-shreyas@users.noreply.github.com>
2021-12-18 11:58:23 +0530

    new commit

---
+++ 37_itsALeapYear.js
@@ -0,0 +1,16 @@
+const isLeap = (year) => {
+  // code here
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
