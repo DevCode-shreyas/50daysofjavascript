5b15cf53795bb6bc1028260480e53d9b92e2d7bc
shreyas bole <68109462+DevCode-shreyas@users.noreply.github.com>
2021-12-19 22:17:58 +0530

    initial commit

---
+++ 39_MixedJuicesInstructions.js
@@ -0,0 +1,16 @@
+const timeToMixJuice = (name) => {
+  // code here
+  return;
+};
+
+const limesToCut = (wedgesNeeded, limes) => {
+  // code here
+
+  return;
+};
+
+const remainingOrders = (timeLeft, orders) => {
+  // code here
+
+  return;
+};
