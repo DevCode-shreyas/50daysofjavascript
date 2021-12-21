71c13b036820ec50e5d741737224b5c84ef1cd93
shreyas bole <68109462+DevCode-shreyas@users.noreply.github.com>
2021-12-21 11:40:21 +0530

    initial commit

--- abc.js
+++
@@ -1,8 +0,0 @@
-function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
-
-
-	return Math.random() * (rangeEnd - rangeStart) + rangeStart;
-    
-}
-
-console.log(`My random number: ${randomNumberGeneratorInRange(5, 100)}`)
